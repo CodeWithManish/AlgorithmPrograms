adb5e7cc2da1bbbba3e793d7ea289c8243b74a39
Manish <manishkumar0156@gmail.com>
2022-02-15 14:56:58 +0530

    [Manish] Add: Insertion Sort Operation

--- AlgorithmProgram/Program.cs
+++ AlgorithmProgram/Program.cs
@@ -10,7 +10,23 @@ namespace AlgorithmProgram
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Algorithm Programs!");
+            Console.WriteLine("Welcome to Algorithm Programs!\n");
+            Console.WriteLine("Enter 1.to print insertion Sort\n");
+            bool flag = true;
+            while (flag)
+            {
+                int check = Convert.ToInt32(Console.ReadLine());
+                switch (check)
+                {
+                    case 1:
+                        InsertionSort insertion= new InsertionSort();
+                        int[] arr = { 20, 15, 4, 8, 12, 3 };
+                        insertion.Sort(arr);
+                        insertion.printArray(arr);
+                        break;
+                }
+            }
+
         }
     }
 }
