ac00c18657e5313dea7820eb52572e216e1b1323
Manish <manishkumar0156@gmail.com>
2022-02-15 21:57:14 +0530

    [Manish] Add: range of prime Number

---
+++ AlgorithmProgram/PrimeNumRange.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmProgram
+{
+    public class PrimeNumRange
+    {
+
+        public void FindPrime()
+        {
+            int count = 0; int range = 1000;
+            for (int i = 1; i < range; i++)
+            {
+                for (int j = 1; j < range; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        count++;
+                    }
+                }
+                if (count == 2)
+                {
+                    Console.Write(i + " ");
+                }
+                count = 0;
+            }
+
+        }
+    }
+}
--- AlgorithmProgram/Program.cs
+++ AlgorithmProgram/Program.cs
@@ -12,7 +12,7 @@ namespace AlgorithmProgram
         {
             Console.WriteLine("Welcome to Algorithm Programs!\n");
             Console.WriteLine("Enter 1.to print insertion Sort\nEnter 2.to print Bubble Sort\n" +
-                "Enter 3.to pirnt Merge Sort\nEnter 4.to print Anagram\n");
+                "Enter 3.to pirnt Merge Sort\nEnter 4.to print Anagram\nEnter 5.to print Range of Prime Number\n");
             bool flag = true;
             while (flag)
             {
@@ -33,7 +33,7 @@ namespace AlgorithmProgram
                         break;
 
                     case 3:
-                        int[] arr2 = { 33, 12, 9, 85, 21, 4 };
+                        int[] arr2 = { 33, 12, 9, 85, 21, 4 }; 
                         MergeSort.Sort(arr2, 0, arr2.Length - 1);
                         MergeSort.PrintArray(arr2);
                         break;
@@ -42,8 +42,12 @@ namespace AlgorithmProgram
                         Anagram anagram = new Anagram();
                         anagram.FindAnagram();
                         break;
-                  
-                    
+
+                    case 5:
+                        PrimeNumRange prime = new PrimeNumRange();
+                        prime.FindPrime();
+                        break;
+
                 }
             }
 
