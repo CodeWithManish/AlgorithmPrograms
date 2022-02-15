d6634907a8ac7b801e9fba4c7b1259c8ea0ff698
Manish <manishkumar0156@gmail.com>
2022-02-15 15:21:51 +0530

    [Manish] Add: Bubble sort Operation

--- AlgorithmProgram/Program.cs
+++ AlgorithmProgram/Program.cs
@@ -11,7 +11,7 @@ namespace AlgorithmProgram
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Algorithm Programs!\n");
-            Console.WriteLine("Enter 1.to print insertion Sort\n");
+            Console.WriteLine("Enter 1.to print insertion Sort\nEnter 2.to print Bubble Sort\n");
             bool flag = true;
             while (flag)
             {
@@ -23,6 +23,12 @@ namespace AlgorithmProgram
                         int[] arr = { 20, 15, 4, 8, 12, 3 };
                         insertion.Sort(arr);
                         insertion.printArray(arr);
+                        break;
+
+                        case 2:
+                        int[] arr1 = { 10, 35, 32, 13, 26 };
+                        BubbleSort.BubbleSortInArray(arr1);
+                        BubbleSort.PrintArray(arr1);
                         break;
                 }
             }
